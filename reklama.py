6c464fb1dc283bac4f5fef4de944cf48ec020710
thekla1ny <96620706+thekla1ny@users.noreply.github.com>
2025-07-23 18:52:36 +0500

    Update reklama.py

--- reklama.py
+++ reklama.py
@@ -1,18 +1,21 @@
 from pyrogram import Client
 import asyncio
 
-TARGET_CHAT_ID = -1001984966725
+# Константы
 SOURCE_CHAT_ID = "me"
+TARGET_CHAT_ID = -1001984966725
 
+# Ввод данных
 print("авторизация через телеграм. надо ввести данные. Создал @KingOfInsanity")
-
 api_id = int(input("Введи свой API ID: "))
 api_hash = input("Введи свой API HASH: ")
 session_name = "akkaunt_data"
 
+# Клиент
 app = Client(session_name, api_id=api_id, api_hash=api_hash)
 
-async def forward_last_message(app, SOURCE_CHAT_ID, TARGET_CHAT_ID):
+# Основная функция
+async def forward_last_message():
     async for message in app.get_chat_history(SOURCE_CHAT_ID, limit=1):
         if message:
             await app.forward_messages(
@@ -24,7 +27,8 @@ async def forward_last_message(app, SOURCE_CHAT_ID, TARGET_CHAT_ID):
         else:
             print("нет сообщений в избранном.")
 
-async def main_loop():
+# Главный цикл
+async def main():
     async with app:
         print("авторизован. рассылка начата.")
         while True:
